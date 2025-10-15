b5902295c18d813ce792a7922f64595a22fb0b5c
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2025-10-15 16:49:14 +0300

    Migrate to Vite and update to bootstrap 5

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
+});
