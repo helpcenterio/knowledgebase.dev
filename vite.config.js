906e99271e9ee5a985079c72a06abdd2e2ec2137
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2025-12-05 11:33:48 +0200

    Migrate fully to vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: process.cwd(),
   base: './',
   build: {
     outDir: 'dist',
-    emptyOutDir: false,
+    emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(process.cwd(), 'index.html')
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        article: resolve(__dirname, 'pages/article.html'),
+        category: resolve(__dirname, 'pages/category.html'),
+        contacts: resolve(__dirname, 'pages/contacts.html'),
+        search: resolve(__dirname, 'pages/search.html')
+      }
     }
   }
 });
